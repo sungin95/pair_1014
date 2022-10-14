faca8a8b5372a99137b4c30e299a2638f77e4ffe
hvvany <kjunhwan1998@gmail.com>
2022-10-14 13:20:25 +0900

    index 회원목록

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,17 @@
+from webbrowser import get
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login as auth_login
+from django.contrib.auth import get_user_model
 
 # Create your views here.
 def index(request):
-    return render(request, "accounts/index.html")
+    users = get_user_model().objects.all()
+    context = {
+        'users':users,
+    }
+    return render(request, "accounts/index.html",context)
 
 
 def signup(request):
