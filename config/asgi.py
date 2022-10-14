180522757c36ebeffaaf664ca096d326d2561d58
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2022-10-14 09:20:17 +0900

    프로젝트명 수정

--- config/asgi.py
+++ config/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for a_pjt_login project.
+ASGI config for config project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'a_pjt_login.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_asgi_application()
