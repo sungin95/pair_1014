2a1ab58765c881f7ac009e90954cfa7a11affa3f
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2022-10-16 11:25:26 +0900

    test

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,14 +1,18 @@
-from dataclasses import field, fields
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
-        fields = ("username","email")
+        fields = ("username", "email")
+
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
         model = get_user_model()
-        fields = ('username', 'email',)
+        fields = (
+            "username",
+            "email",
+        )
--- accounts/views.py
+++ accounts/views.py
@@ -11,9 +11,9 @@ from django.contrib.auth.decorators import login_required
 def index(request):
     users = get_user_model().objects.all()
     context = {
-        'users':users,
+        "users": users,
     }
-    return render(request, "accounts/index.html",context)
+    return render(request, "accounts/index.html", context)
 
 
 def signup(request):
@@ -34,9 +34,9 @@ def signup(request):
 
 def login(request):
     if request.user.is_authenticated:
-        return redirect('accounts:index')
+        return redirect("accounts:index")
     if request.method == "POST":
-        form = AuthenticationForm(request, data=request.POST)
+        form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             auth_login(request, form.get_user())
             return redirect(request.GET.get("next") or "accounts:index")
@@ -47,42 +47,46 @@ def login(request):
     }
     return render(request, "accounts/login.html", context)
 
+
 def logout(request):
     auth_logout(request)
-    return redirect('accounts:index')
+    return redirect("accounts:login")
+
 
 @login_required
 def detail(request, pk):
     user = get_user_model().objects.get(pk=pk)
-    context={
-        'user':user,
+    context = {
+        "user": user,
     }
-    return render(request, 'accounts/detail.html', context)
+    return render(request, "accounts/detail.html", context)
+
 
 @login_required
 def update(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CustomUserChangeForm(request.POST, instance=request.user)
         if form.is_valid():
             form.save()
-            return redirect('accounts:index')
+            return redirect("accounts:index")
     else:
         form = CustomUserChangeForm(instance=request.user)
     context = {
-        'form':form,
+        "form": form,
     }
-    return render(request, 'accounts/update.html', context)
+    return render(request, "accounts/update.html", context)
+
 
 @login_required
 def change_password(request):
-    if request.method =='POST':
+    if request.method == "POST":
         form = PasswordChangeForm(request.user, request.POST)
         if form.is_valid():
             form.save()
-            return redirect('accounts:index')
+            return redirect("accounts:index")
     else:
         form = PasswordChangeForm(request.user)
     context = {
-        'form':form,
+        "form": form,
     }
-    return render(request, 'accounts/change_password.html', context)
+    return render(request, "accounts/change_password.html", context)
